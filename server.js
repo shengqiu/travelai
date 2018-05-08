e94ca54b1b3965677267db916c6be011a8c39bfa
shengqiu <shengqiu.purdue@gmail.com>
2018-05-07 22:19:53 -0700

    add trip pug file

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var pug = require("pug");
 
+app.set('view engine', 'pug');
+app.use("/style", express.static(path.join(__dirname + '/style')));
 app.use("/gfx", express.static(path.join(__dirname + '/gfx')));
 app.use("/lib", express.static(path.join(__dirname + '/lib')));
 
 
+const compileFunction = pug.compileFile(
+    path.join(__dirname, 'template/trip.pug')
+)
+
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/template/index.html'));
+    res.sendFile(path.join(__dirname, '/template/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
 app.get('/trip',function(req,res){
-    res.sendFile(path.join(__dirname+'/template/trip.html'));
+    res.render(path.join(__dirname, '/template/trip.pug'));
     //__dirname : It will resolve to your project folder.
 });
 
